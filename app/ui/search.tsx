93ccb130c189d5c08645e1dbb44c27204312a10d
mark on github <markongithub@users.noreply.github.com>
2025-02-11 19:38:22 -0500

    this updates the URL query param on every keystroke

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   function handleSearch(term: string) {
-    console.log(term);
+    const params = new URLSearchParams(searchParams); // this uses the current ones from the browser?
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+
   }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
