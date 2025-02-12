3bd4e6bd9cc741841cd6bf89413c153880d1daee
mark on github <markongithub@users.noreply.github.com>
2025-02-11 19:53:42 -0500

    Do all the pagination code except the big uncommenting

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,10 +12,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams); // this uses the current ones from the browser?
+    params.set('page', '1');
+
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
+      params.delete('page');
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
